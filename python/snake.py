7c18faaf2b1d97e4831a7f8551862aa4e0546d24
Massa Adonaï <massaadonai@gmail.com>
2024-10-12 19:17:06 +0200

    Premier Fonction

--- python/snake.py
+++ python/snake.py
@@ -29,3 +29,17 @@ TAILLE_SEGMENT = 10
 # Police pour le score
 font_style = pygame.font.SysFont("arial", 25)
 score_font = pygame.font.SysFont("arila black", 35)
+
+# Dessine le serpent
+def DESSINER_SERPENT(TAILLE_SEGMENT, serpent):
+    for x in serpent:
+        pygame.draw.rect(fenetre, VERT, [x[0], x[1], TAILLE_SEGMENT, TAILLE_SEGMENT])
+
+# Afficher le score     
+def AFFICHER_SCORE(score):
+    valeur = score_font.render("Score: " + str(score), True, NOIR)
+    fenetre.blit(valeur, [0, 0])
+
+def MESSAGE(msg, couleur):
+    mesg = font_style.render(msg, True, couleur)
+    fenetre.blit(mesg, [LARGUR / 6, HAUTEUR / 3])
